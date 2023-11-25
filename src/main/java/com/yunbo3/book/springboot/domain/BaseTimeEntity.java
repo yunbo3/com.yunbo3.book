962e7600869c0a32ca48be5bacd52da2ca02f7a5
yunbo3 <yunbo3@naver.com>
2023-11-25 19:35:17 +0900

    JPA Auditing

--- src/main/java/com/yunbo3/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/yunbo3/book/springboot/domain/BaseTimeEntity.java
@@ -14,6 +14,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
+
     @CreatedDate
     private LocalDateTime createdDate;
 
