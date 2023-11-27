34d619875aa0e7b9498d64c8a38fdd53244d9a28
yunbo3 <yunbo3@naver.com>
2023-11-27 23:16:36 +0900

    p139

---
+++ src/test/java/com/yunbo3/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,30 @@
+package com.yunbo3.book.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @WithMockUser(username = "test", password = "{noop}test", roles = "USER")
+    @Test
+    public void 메인페이지_로딩() {
+        // when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        // then
+        assertThat(body.trim()).contains("스프링부트로 시작하는 웹 서비스");
+    }
+}
